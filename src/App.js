de574f6ac3ad931c0173f253e1b614caaca576c6
Felipe Quintero <ffquint@gmail.com>
2021-01-05 20:17:01 -0500

    forgot to fork original project repo. Forked and cloned, moved code to correct repo/branch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
 import "./App.css";
+import Details from './components/Details'
 
 function App() {
+  const [photo, setPhoto] = useState([])
+
+  useEffect(() => {
+    console.log('making an api request!')
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then(response => console.log(response.data))
+      .catch(err => console.log(err))
+  }, [])
+
+
   return (
     <div className="App">
       <p>
---
+++ src/components/Details.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
+import Photo from './Photo'
+
+export default function Details(props) {
+    const { date, explanation } = props
+
+
+}
