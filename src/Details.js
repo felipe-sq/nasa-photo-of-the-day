156e9cb9a88fcab5cca9564ff1190478ca159c49
Felipe Quintero <ffquint@gmail.com>
2021-03-02 15:51:50 -0500

    working on code for App.js and Details.js

---
+++ src/Details.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
+import Photo from './Photo'
+
+export default function Details(props) {
+    const { photo, date, explanation } = props
+    
+    return (
+        <div>
+            <h2>Date: {</h2>
+
+        </div>
+
+    )
+}
