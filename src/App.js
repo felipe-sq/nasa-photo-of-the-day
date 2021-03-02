95997e0b8ae3ada60b31682b2ff500f18424b727
Felipe Quintero <ffquint@gmail.com>
2021-03-02 15:58:12 -0500

    working out error message: module not found - can't resolve 'axios' in Details.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios'
+// import axios from 'axios'
 import "./App.css";
 import Details from "./Details"
 
 function App() {
-  const [photo, setPhoto] = useState([])
+  // const [photo, setPhoto] = useState([])
 
-  useEffect(() => {
-    console.log('making an api request!')
-    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-      .then(response => console.log(response.data))
-      .catch(err => console.log(err))
-  }, [])
-  
+  // useEffect(() => {
+  //   console.log('making an api request!')
+  //   axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+  //     .then(response => console.log(response.data))
+  //     .catch(err => console.log(err))
+  // }, [])
+
   return (
     <div className="App">
       <p>
--- src/Details.js
+++ src/Details.js
@@ -1,13 +1,21 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios'
-import Photo from './Photo'
 
 export default function Details(props) {
     const { photo, date, explanation } = props
-    
+    const [photos, setPhoto] = useState([])
+
+    useEffect(() => {
+      console.log('making an api request!')
+      axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+        .then(response => console.log(response.data))
+        .catch(err => console.log(err))
+    }, [])
+
     return (
         <div>
-            <h2>Date: {</h2>
+            <img>{photo}</img>
+            <h2>Date: {}</h2>
 
         </div>
 
